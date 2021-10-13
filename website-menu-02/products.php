3a8fc15bd72292fb912efd71fff3ebb194d60e56
ashhalliday14 <87982373+ashhalliday14@users.noreply.github.com>
2021-10-13 09:16:04 +0100

    mad changes bro

---
+++ website-menu-02/products.php
@@ -0,0 +1,137 @@
+<!-- <!doctype html>
+<html lang="en">
+    <?php
+        //includes
+        //include 'connection.php';
+    ?>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="website-menu-02">
+
+    </head>
+
+    <body>
+        <div class="products">
+            <?php
+               // $sql = "SELECT * 
+                        //FROM   tblProduct";
+                //$result = mysqli_query($conn, $sql);
+
+                //while($row = mysqli_fetch_assoc($result))
+                //{
+                    //echo "ID: ".$row["ProductID"]."<br>";
+                   // echo "Name: ".$row["Name"]."<br>";
+                    //echo "Description: ".$row["Description"]."<br>";
+                    //echo "Price: ".$row["Price"]."<br>";
+                    //echo "<a href='view_product_details.php?id=".$row["ProductID"]."&add=true'>View Further Details</a><br>";
+                //}
+            ?>
+        </div>
+    </body>
+</html> -->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Products</title>
+    <?php
+        //includes
+        include 'connection.php';
+    ?>
+  </head>
+  <body>
+
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+            <div class="col-3">
+              <div class="site-logo">
+                <a href="index.html" class="font-weight-bold">Dippin' Donuts</a>
+              </div>
+            </div>
+
+            <div class="col-9  text-right">
+              
+
+              <span class="d-inline-block d-lg-none"><a href="#" class="text-primary site-menu-toggle js-menu-toggle py-5"><span class="icon-menu h3 text-white"></span></a></span>
+
+
+              <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav ml-auto ">
+                  <li class="active"><a href="products.php" class="nav-link">Products</a></li>
+                  <li><a href="#" class="nav-link">Shopping Cart</a></li>
+                  <li><a href="#" class="nav-link">Login</a></li>
+                  <li><a href="#" class="nav-link">Register</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            
+          </div>
+        </div>
+
+      </header>
+
+    <div class="hero"></div>
+  
+    <body>
+        <div class="products">
+            <?php
+                $sql = "SELECT * 
+                        FROM   tblProduct";
+                $result = mysqli_query($conn, $sql);
+
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    echo "ID: ".$row["ProductID"]."<br>";
+                    echo "Name: ".$row["Name"]."<br>";
+                    echo "Description: ".$row["Description"]."<br>";
+                    echo "Price: ".$row["Price"]."<br>";
+                    echo "<a href='view_product_details.php?id=".$row["ProductID"]."&add=true'>View Further Details</a><br>";
+                }
+            ?>
+        </div>
+    </body>
+  
+    
+    
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
